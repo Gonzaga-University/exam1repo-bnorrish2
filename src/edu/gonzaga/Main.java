7b44f6a6cac3b86ca30d2e3791677cf14ba1925f
Brady Norrish <bnorrish@zagmail.gonzaga.edu>
2020-10-13 15:34:05 -0700

    added product function and print function to adder class, and modified access to some fields

--- src/edu/gonzaga/Main.java
+++ src/edu/gonzaga/Main.java
@@ -1,17 +1,28 @@
 package edu.gonzaga;
 
 class Adder {
-    int val1;
-    int val2;
+    private int val1;
+    private int val2;
 
     public Adder(int new_val1, int new_val2) {
         val1 = new_val1;
         val2 = new_val2;
     }
 
-    public int addVals()
+    private int addVals()
     {
         return val1 + val2;
+    }
+
+    public int multVals()
+    {
+        return val1 * val2;
+    }
+
+    private void printResults()
+    {
+        System.out.println("Sum: " + addVals());
+        System.out.println("Product: " + multVals());
     }
 
 }
